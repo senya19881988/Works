a2c1cf70e5377d94411033264a759f189091849e
senya19881988 <63978086+senya19881988@users.noreply.github.com>
2022-06-23 14:10:56 +0300

    Добавление описания действий в код

--- Python_calc.py
+++ Python_calc.py
@@ -1,5 +1,7 @@
+# Импортируем оболочку калькулятора
 import tkinter as tk
 
+# Создаем окно Tkinter с размером 300х500 и названием Python калькулятор
 window=tk.Tk()
 window.geometry("300x500")
 window.title("Python калькулятор")
